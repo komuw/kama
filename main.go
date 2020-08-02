69b786a786f8da5914141ec8bd681169fa707db0
komuW <komuw05@gmail.com>
2020-08-02 20:50:35 +0300

    add tests for packages

--- main.go
+++ main.go
@@ -24,8 +24,6 @@ import (
 
 // TODO: this will stutter; `dir.dir(23)`
 // maybe it is okay??
-// TODO: surface all info for both the type and its pointer.
-// currently `dir(&http.Client{})` & `dir(http.Client{})` produces different output; they should NOT
 func dir(i interface{}) {
 	var res interface{}
 	var err error
