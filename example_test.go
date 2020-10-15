c0adbfad6c74caeeaa19619dedb7620d0a0718a1
komuW <komuw05@gmail.com>
2020-10-15 09:42:15 +0300

    re-org

--- example_test.go
+++ example_test.go
@@ -4,9 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"testing"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/komuw/kama"
 )
 
@@ -25,7 +23,7 @@ func ExampleDirp() {
 	// KIND: struct
 	// SIGNATURE: [kama_test.myHandler *kama_test.myHandler]
 	// FIELDS: [
-	// 	Logger
+	// 	Logger 
 	// 	]
 	// METHODS: [
 	// 	ServeHTTP func(*kama_test.myHandler, http.ResponseWriter, *http.Request)
@@ -35,40 +33,4 @@ func ExampleDirp() {
 	//    Logger: &Logger{},
 	// }
 	// ]
-}
-
-func TestVars(t *testing.T) {
-	tt := []struct {
-		variable interface{}
-		expected string
-	}{
-		{
-			myHandler{Logger: log.New(os.Stderr, "", 0)}, `
-[
-NAME: github.com/komuw/kama_test.myHandler
-KIND: struct
-SIGNATURE: [kama_test.myHandler *kama_test.myHandler]
-FIELDS: [
-	Logger 
-	]
-METHODS: [
-	ServeHTTP func(*kama_test.myHandler, http.ResponseWriter, *http.Request) 
-	ServeHTTP func(kama_test.myHandler, http.ResponseWriter, *http.Request) 
-	]
-SNIPPET: myHandler{
-  Logger: &Logger{},
-}
-]
-`,
-		},
-	}
-
-	for _, v := range tt {
-		res := kama.Dir(v.variable)
-
-		if !cmp.Equal(res, v.expected) {
-			diff := cmp.Diff(v.expected, res)
-			t.Errorf("\ngot: \n\t%#+v \nwanted: \n\t%#+v \ndiff: \n======================\n%s\n======================\n", res, v.expected, diff)
-		}
-	}
 }
