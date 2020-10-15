fc5f78f80b36984604f495ad82c2dadb58b9ed2d
komuW <komuw05@gmail.com>
2020-10-15 10:10:14 +0300

    re-org

--- example_test.go
+++ example_test.go
@@ -3,9 +3,6 @@ package kama_test
 import (
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/komuw/kama"
 )
 
 type myHandler struct{ Logger *log.Logger }
@@ -13,24 +10,27 @@ type myHandler struct{ Logger *log.Logger }
 func (h myHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
 }
 
-func ExampleDirp() {
-	h := myHandler{Logger: log.New(os.Stderr, "", 0)}
-	kama.Dirp(h)
+// This is blocked on:
+// 1. https://github.com/golang/go/issues/41980
+// 2. https://github.com/golang/go/issues/5128#issuecomment-708940093
+// func ExampleDirp() {
+// 	h := myHandler{Logger: log.New(os.Stderr, "", 0)}
+// 	kama.Dirp(h)
 
-	// Output:
-	// [
-	// NAME: github.com/komuw/kama_test.myHandler
-	// KIND: struct
-	// SIGNATURE: [kama_test.myHandler *kama_test.myHandler]
-	// FIELDS: [
-	// 	Logger
-	// 	]
-	// METHODS: [
-	// 	ServeHTTP func(*kama_test.myHandler, http.ResponseWriter, *http.Request)
-	// 	ServeHTTP func(kama_test.myHandler, http.ResponseWriter, *http.Request)
-	// 	]
-	// SNIPPET: myHandler{
-	//    Logger: &Logger{},
-	// }
-	// ]
-}
+// 	// Output:
+// 	// [
+// 	// NAME: github.com/komuw/kama_test.myHandler
+// 	// KIND: struct
+// 	// SIGNATURE: [kama_test.myHandler *kama_test.myHandler]
+// 	// FIELDS: [
+// 	// 	Logger
+// 	// 	]
+// 	// METHODS: [
+// 	// 	ServeHTTP func(*kama_test.myHandler, http.ResponseWriter, *http.Request)
+// 	// 	ServeHTTP func(kama_test.myHandler, http.ResponseWriter, *http.Request)
+// 	// 	]
+// 	// SNIPPET: myHandler{
+// 	//    Logger: &Logger{},
+// 	// }
+// 	// ]
+// }
