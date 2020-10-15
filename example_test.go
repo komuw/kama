e7f99ce7d54f73e99732423022948335cc60bb34
Komu Wairagu <komuw05@gmail.com>
2020-10-15 10:21:12 +0300

    Add example test (#13)
    What:
    - Add example test. 
    That test example is kind of blocked on;
    1. https://github.com/golang/go/issues/41980       
    2. https://github.com/golang/go/issues/5128#issuecomment-708940093

--- example_test.go
+++ example_test.go
@@ -1,72 +1,36 @@
 package kama_test
 
-import (
-	"log"
-	"net/http"
-	"os"
-	"testing"
+// import (
+// 	"log"
+// 	"net/http"
+// )
 
-	"github.com/google/go-cmp/cmp"
-	"github.com/komuw/kama"
-)
+// type myHandler struct{ Logger *log.Logger }
 
-type myHandler struct{ Logger *log.Logger }
-
-func (h myHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
-}
-
-// func ExampleDir() {
-// 	h := myHandler{Logger: log.New(os.Stderr, "", 0)}
-// 	kama.Dir(h)
-// 	// kama.Dir("compress/flate")
-// 	// kama.Dir(&http.Request{})
-// 	// kama.Dir(http.Request{})
-// 	// kama.Dir("github.com/pkg/errors")
-
-// 	// Output:
-// 	//[
-// 	//NAME: github.com/komuw/kama_test.myHandler
-// 	//KIND: struct
-// 	//SIGNATURE: [kama_test.myHandler *kama_test.myHandler]
-// 	//FIELDS: [
-// 	//	Logger
-// 	//	]
-// 	//METHODS: []
-// 	//]
+// func (h myHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
 // }
 
-func TestVars(t *testing.T) {
-	tt := []struct {
-		variable interface{}
-		expected string
-	}{
-		{
-			myHandler{Logger: log.New(os.Stderr, "", 0)}, `
-[
-NAME: github.com/komuw/kama_test.myHandler
-KIND: struct
-SIGNATURE: [kama_test.myHandler *kama_test.myHandler]
-FIELDS: [
-	Logger 
-	]
-METHODS: [
-	ServeHTTP func(*kama_test.myHandler, http.ResponseWriter, *http.Request) 
-	ServeHTTP func(kama_test.myHandler, http.ResponseWriter, *http.Request) 
-	]
-SNIPPET: myHandler{
-  Logger: &Logger{},
-}
-]
-`,
-		},
-	}
+// This is blocked on:
+// 1. https://github.com/golang/go/issues/41980
+// 2. https://github.com/golang/go/issues/5128#issuecomment-708940093
+// func ExampleDirp() {
+// 	h := myHandler{Logger: log.New(os.Stderr, "", 0)}
+// 	kama.Dirp(h)
 
-	for _, v := range tt {
-		res := kama.Dir(v.variable)
-
-		if !cmp.Equal(res, v.expected) {
-			diff := cmp.Diff(v.expected, res)
-			t.Errorf("\ngot: \n\t%#+v \nwanted: \n\t%#+v \ndiff: \n======================\n%s\n======================\n", res, v.expected, diff)
-		}
-	}
-}
+// 	// Output:
+// 	// [
+// 	// NAME: github.com/komuw/kama_test.myHandler
+// 	// KIND: struct
+// 	// SIGNATURE: [kama_test.myHandler *kama_test.myHandler]
+// 	// FIELDS: [
+// 	// 	Logger
+// 	// 	]
+// 	// METHODS: [
+// 	// 	ServeHTTP func(*kama_test.myHandler, http.ResponseWriter, *http.Request)
+// 	// 	ServeHTTP func(kama_test.myHandler, http.ResponseWriter, *http.Request)
+// 	// 	]
+// 	// SNIPPET: myHandler{
+// 	//    Logger: &Logger{},
+// 	// }
+// 	// ]
+// }
