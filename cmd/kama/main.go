61ab765373430cbf835adfccabf70eb1a720c9c9
komuW <komuw05@gmail.com>
2020-10-17 23:18:38 +0300

    m

--- cmd/kama/main.go
+++ cmd/kama/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"net/http"
+	"flag"
+	"fmt"
+	"os"
 
 	"github.com/komuw/kama"
 )
-
-func main() {
-	kama.Dirp("archive/tar")
-	kama.Dirp("compress/flate")
-	kama.Dirp(&http.Request{})
-	kama.Dirp(http.Request{})
-	kama.Dirp("github.com/pkg/errors")
-}
 
 // TODO: clean up
 
@@ -23,3 +17,34 @@ func main() {
 // TODO: add a command line api.
 //   eg; `kama http.Request` or `kama http`
 // have a look at `golang.org/x/tools/cmd/godex`
+
+func usage() {
+	fmt.Fprintf(os.Stderr,
+		`kama prints exported information of types, variables, packages, modules, imports etc.
+It also pretty prints data structures.
+
+examples:
+	kama github.com/pkg/errors
+	    prints exported information of the package(github.com/pkg/errors)
+	kama fmt.Printf
+	    prints exported information of the package object(fmt.Printf)
+	kama http.Response
+	    prints exported information of the package object(http.Response)`)
+	flag.PrintDefaults()
+	os.Exit(0)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		// TODO: suppport more args.
+		// see: https://github.com/golang/tools/blob/gopls/v0.5.1/cmd/godex/godex.go#L58
+		fmt.Fprintln(os.Stderr, "error: number of cli arguments should be one")
+		os.Exit(2)
+	}
+
+	args := flag.Args()
+	kama.Dirp(args[0])
+}
