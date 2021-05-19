140e71012bbc1c39ef533970df32a35fbcad6f79
komuW <komuw05@gmail.com>
2021-05-19 23:06:36 +0300

    handler arrays

--- kama.go
+++ kama.go
@@ -46,12 +46,25 @@ func Dir(i interface{}) string {
 		i := i.(string)
 		res, err := newPak(i)
 		if err != nil {
-			if strings.Contains(err.Error(), "is not in GOROOT") || strings.Contains(err.Error(), "cannot find module") || strings.Contains(err.Error(), "is replaced but not required") || strings.Contains(err.Error(), "is replaced but not required") || strings.Contains(err.Error(), "no required module") || strings.Contains(err.Error(), "to add it:") || strings.Contains(err.Error(), "but not at required version") || strings.Contains(err.Error(), "missing module") {
+			for _, eMsg := range []string{
 				// We check if it is truly a module error. We check all the errors that can be returned.
 				// Unfortunately `ImportMissingError` is an internal error so we cant use errors.Is/As
 				// https://github.com/golang/go/blob/go1.16.4/src/cmd/go/internal/modload/import.go#L49-L81
-				return err.Error()
+				//
+				// This list will need to be kept uptodate with Go versions
+				"is not in GOROOT",
+				"cannot find module",
+				"is replaced but not required",
+				"no required module",
+				"to add it:",
+				"but not at required version",
+				"missing module",
+			} {
+				if strings.Contains(err.Error(), eMsg) {
+					return err.Error()
+				}
 			}
+
 			// If it is not a module error, then probably `i` is a variable of type string.
 			// Thus we need to create a `kama.vari`
 			res := newVari(i)
