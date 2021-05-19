ff9604aafc9ca13f7e49738ab62e01088f4563fc
komuW <komuw05@gmail.com>
2021-05-19 22:54:53 +0300

    handler arrays

--- kama.go
+++ kama.go
@@ -14,6 +14,7 @@ package kama
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 // Dirp prints exported information of types, variables, packages, modules, imports
@@ -45,6 +46,14 @@ func Dir(i interface{}) string {
 		i := i.(string)
 		res, err := newPak(i)
 		if err != nil {
+			if strings.Contains(err.Error(), "is not in GOROOT") || strings.Contains(err.Error(), "cannot find module") || strings.Contains(err.Error(), "is replaced but not required") || strings.Contains(err.Error(), "is replaced but not required") || strings.Contains(err.Error(), "no required module") || strings.Contains(err.Error(), "to add it:") || strings.Contains(err.Error(), "but not at required version") || strings.Contains(err.Error(), "missing module") {
+				// We check if it is truly a module error. We check all the errors that can be returned.
+				// Unfortunately `ImportMissingError` is an internal error so we cant use errors.Is/As
+				// https://github.com/golang/go/blob/go1.16.4/src/cmd/go/internal/modload/import.go#L49-L81
+				return err.Error()
+			}
+			// If it is not a module error, then probably `i` is a variable of type string.
+			// Thus we need to create a `kama.vari`
 			res := newVari(i)
 			return res.String()
 		}
