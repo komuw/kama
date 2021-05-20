f7b98e14e1aab14819bc6ee3f65a89b2dd739e2e
komuW <komuw05@gmail.com>
2021-05-20 08:19:28 +0300

    update

--- vars_test.go
+++ vars_test.go
@@ -85,7 +85,7 @@ func TestBasicVariables(t *testing.T) {
 				Signature: []string{"[10000]int"},
 				Fields:    []string{},
 				Methods:   []string{},
-				Val:       "[10000]int{\n  0,\n  1,\n  2,\n  3,\n  4,\n  5,\n  6,\n  7,\n  8,\n  9,\n  10,\n  11,\n  12,\n  13,\n  14,\n  15,\n  16,\n  17,\n  18,\n  19\n...<snipped>.."},
+				Val:       "[10000]int{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1 ...<snipped>.."},
 		},
 
 		{
