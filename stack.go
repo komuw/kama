f85b6d2e87666746acfc2e3382356ff5b5d4e0e2
$MY_NAME <komuw05@gmail.com>
2021-12-05 21:40:41 +0300

    tab

--- stack.go
+++ stack.go
@@ -1,9 +1,10 @@
 package kama
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"runtime/debug"
+	"runtime"
 	"strings"
 )
 
@@ -49,39 +50,112 @@ func printWithColor(s string, color string, bold bool) {
 }
 
 func stackp() {
-	x := debug.Stack()
+	// x := debug.Stack()
 
-	lines := [][]byte{}
-	curLine := []byte{}
-	for _, v := range x {
-		if v == 10 {
-			lines = append(lines, curLine)
-			curLine = []byte{}
-		} else {
-			curLine = append(curLine, v)
+	// lines := [][]byte{}
+	// curLine := []byte{}
+	// for _, v := range x {
+	// 	if v == 10 {
+	// 		lines = append(lines, curLine)
+	// 		curLine = []byte{}
+	// 	} else {
+	// 		curLine = append(curLine, v)
+	// 	}
+	// }
+
+	// tab := "\t"
+	// for _, v := range lines {
+	// 	it := string(v)
+	// 	if strings.Contains(it, "runtime/") {
+	// 		// compiler
+	// 		printWithColor(it, "cyan", false)
+	// 	} else if strings.Contains(it, "[running]") {
+	// 		// treat it like compiler
+	// 		printWithColor(it, "cyan", false)
+	// 	} else if strings.Contains(it, "github.com/komuw/") {
+	// 		// third party
+	// 		printWithColor(it, "magenta", false)
+	// 	} else if strings.Contains(it, "/home/komuw") {
+	// 		// your code
+	// 		printWithColor(tab+it, "green", true)
+	// 	} else {
+	// 		printWithColor(tab+it, "green", false)
+	// 	}
+	// }
+
+	// curLine = nil
+	// lines = nil
+
+	fmt.Println(getStackTrace())
+}
+
+const maxStackLength = 50
+
+// frm is like a runtime.Frame
+type frm struct {
+	file     string
+	line     int
+	function string
+}
+
+func getStackTrace() string {
+	stackBuf := make([]uintptr, maxStackLength)
+	length := runtime.Callers(4, stackBuf[:])
+	stack := stackBuf[:length]
+
+	frames := runtime.CallersFrames(stack)
+
+	var frms []frm
+	for {
+		frame, more := frames.Next()
+		frms = append(frms, frm{file: frame.File, line: frame.Line, function: frame.Function})
+		if !more {
+			break
 		}
 	}
 
-	tab := "\t"
-	for _, v := range lines {
-		it := string(v)
-		if strings.Contains(it, "runtime/") {
-			// compiler
-			printWithColor(it, "cyan", false)
-		} else if strings.Contains(it, "[running]") {
-			// treat it like compiler
-			printWithColor(it, "cyan", false)
-		} else if strings.Contains(it, "github.com/komuw/") {
-			// third party
-			printWithColor(it, "magenta", false)
-		} else if strings.Contains(it, "/home/komuw") {
-			// your code
-			printWithColor(tab+it, "green", true)
-		} else {
-			printWithColor(tab+it, "green", false)
+	txtLast := readLastLine(frms[0].file, int64(frms[0].line))
+
+	trace := ""
+	for k, v := range frms {
+		trace = trace + fmt.Sprintf("\n\t%s:%d %s", v.file, v.line, v.function)
+		if k == 0 && txtLast != "" {
+			trace = trace + "\n" + txtLast
 		}
 	}
 
-	curLine = nil
-	lines = nil
+	return trace
+}
+
+func readLastLine(file string, line int64) string {
+	txt := ""
+
+	f, err := os.Open(file)
+	if err != nil {
+		return txt
+	}
+	defer f.Close()
+
+	// NB: scanner will error if file is larger than scanner.MaxScanTokenSize
+	// which is about 65_000 lines. We should do something about that in the future.
+	scanner := bufio.NewScanner(f)
+
+	curLine := int64(0)
+	for scanner.Scan() {
+		curLine = curLine + 1
+
+		// show the code at that line, including the previous two lines and the next one line.
+		if curLine == line-2 || curLine == line-1 || curLine == line+1 {
+			txt = txt + fmt.Sprintf("%d: %s", curLine, "\t\t") + scanner.Text() + "\n"
+		} else if curLine == line {
+			txt = txt + fmt.Sprintf("%d: %s", curLine, "\t\t") + "--> " + scanner.Text() + "\n"
+		}
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		return txt
+	}
+
+	return strings.TrimSuffix(txt, "\n")
 }
