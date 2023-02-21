9b6b04161dca45e14c76e55a19849a3ae1908728
$MY_NAME <komuw05@gmail.com>
2023-02-21 18:54:21 +0300

    a

--- vars.go
+++ vars.go
@@ -194,16 +194,6 @@ func getFields(iType reflect.Type) []string {
 				continue
 			}
 			name := f.Name + " " + f.Type.String()
-
-			if f.Type.Kind() == reflect.Struct {
-				name = fmt.Sprintf("%s struct {", f.Name)
-				for _, v := range getFields(f.Type) {
-					name = name + v + ", "
-				}
-				name = strings.TrimRight(name, " ,") // remove space & comma
-				name = name + "}"
-			}
-
 			fields = append(fields, name)
 		}
 	}
